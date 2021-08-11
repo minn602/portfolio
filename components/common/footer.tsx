44c581c3853142842ec09467944c024b56017d23
cindy <minn602@gmail.com>
2021-08-12 01:12:24 +0900

    modify: 풋터 레이아웃 수정 완료

---
+++ components/common/footer.tsx
@@ -0,0 +1,12 @@
+import styles from "../../styles/Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.Footer}>
+      <p>© MINSUN JUNG</p>
+      <p>PORTFOLIO &lsquo;21</p>
+    </footer>
+  );
+};
+
+export default Footer;
